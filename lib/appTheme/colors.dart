f733720f0b8f29f57a897a46356430f4e441ab97
01KingMaker <fabrakoto0987@gmail.com>
2024-07-02 19:25:47 +0300

    app theme & police

---
+++ lib/appTheme/colors.dart
@@ -0,0 +1,8 @@
+// lib/colors.dart
+import 'package:flutter/material.dart';
+
+const blueColor = Color(0xff266EF1);
+const blancAttenuer = Color(0xffF8F8F8);
+const cardColor = Color(0xffF8F8F8);
+
+const textColor = Colors.black;
---
+++ lib/appTheme/policy.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const h1 = 24;
+
+const textLabelTextFielStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+const formTitleStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  color: Colors.black87,
+);
