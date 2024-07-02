a41aae2c338b85329b8028357a008f5d807f2fe7
01KingMaker <fabrakoto0987@gmail.com>
2024-07-02 19:26:27 +0300

    test route

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:kandrako/components/annonce/annonce_liste.dart';
+import 'package:kandrako/widgets/annonce/create/annonce_create.dart';
+import 'package:kandrako/widgets/annonce/filtre/annonce_filtre.dart';
+import 'package:kandrako/widgets/annonce/liste/annonce_details.dart';
+import 'package:kandrako/widgets/annonce/liste/annonce_liste.dart';
+import 'package:kandrako/widgets/annonce/my_annonce/annonce_utilisateur.dart';
+import 'package:kandrako/widgets/annonce/my_annonce/annonce_utilisateur_details.dart';
 
-/// Flutter code sample for [Card].
 
-void main() => runApp(const HomePage());
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "KANDRAKO",
-      initialRoute: "/",
-      routes: {
-        "/": (context) => AnnonceListe(),
-      },
-    );
-  }
+void main() {
+  runApp(MaterialApp(  
+    title: "KANDRAKO",
+    initialRoute: "/",
+    routes: {
+      "/": (context) => const AnnonceListe(),
+    },
+  ));
 }
